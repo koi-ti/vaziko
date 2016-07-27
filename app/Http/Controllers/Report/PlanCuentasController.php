b5b03a6453539613f5dd8f0f199966dfc0707a40
Pedro Camargo <dropecamargo@gmail.com>
2016-07-26 21:11:41 -0500

    Esquema reportes - produccion

--- app/Http/Controllers/Report/PlanCuentasController.php
+++ app/Http/Controllers/Report/PlanCuentasController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use Excel, PHPExcel_Style_Fill, PHPExcel_Style_Border;
+use Excel;
 
 use App\Models\Accounting\PlanCuenta;
 
@@ -18,94 +18,30 @@ class PlanCuentasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('reports.accounting.plancuentas.index');
-    }
+        if($request->has('type')) {
+            $query = PlanCuenta::query();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+            // Filters
+            if($request->has('plancuentas_nivel')) {
+                $query->where('plancuentas_nivel', $request->plancuentas_nivel);
+            }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $query = PlanCuenta::query();
+            $query->orderBy('plancuentas_cuenta', 'asc');
+            $plancuentas = $query->get();
 
-        // Filters
-        if($request->has('plancuentas_nivel')) {
-            $query->where('plancuentas_nivel', $request->plancuentas_nivel);
+            Excel::create(sprintf('%s_%s_%s', 'vaziko_plancuentas', date('Y-m-d'), date('H:m:s')), function($excel) use($plancuentas) {
+                $excel->setTitle('Plan de Unico de Cuentas - P.U.C');
+                $excel->setCreator(config('koi.app.name'));
+                $excel->setCompany(config('koi.name'));
+
+                $excel->sheet('Excel', function($sheet) use($plancuentas) {
+                    $sheet->setFontSize(9);
+                    $sheet->loadView('reports.accounting.plancuentas.report', ['plancuentas' => $plancuentas]);
+                });
+            })->download('pdf');
         }
-
-        $query->orderBy('plancuentas_cuenta', 'asc');
-        $plancuentas = $query->get();
-
-        Excel::create(sprintf('%s_%s_%s', 'vaziko_plancuentas', date('Y-m-d'), date('H:m:s')), function($excel) use($plancuentas) {
-            $excel->setTitle('Plan de Unico de Cuentas - P.U.C');
-            $excel->setCreator(config('koi.app.name'));
-            $excel->setCompany(config('koi.name'));
-
-            $excel->sheet('Excel', function($sheet) use($plancuentas) {
-                $sheet->setFontSize(9);
-                $sheet->loadView('reports.accounting.plancuentas.report', ['plancuentas' => $plancuentas]);
-            });
-        })->download('pdf');
-        // })->export('xls');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('reports.accounting.plancuentas.index');
     }
 }
