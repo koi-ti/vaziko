833e77b807b92b4e8c838169598beb576a02f335
Pedro Camargo <dropecamargo@gmail.com>
2017-02-22 23:21:17 -0500

    terceros - produccion

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -411,15 +411,15 @@
 							            <div class="box-body">
 						                    <form method="POST" accept-charset="UTF-8" id="form-item-roles" data-toggle="validator">
 						                        <div class="row">
-						                        	<label for="role_id" class="control-label col-sm-1 col-sm-offset-1 hidden-xs">Rol</label>
-						                            <div class="form-group col-sm-7 col-xs-9">
+						                        	<label for="role_id" class="control-label col-sm-1 col-md-offset-1 hidden-xs">Rol</label>
+						                            <div class="form-group col-md-7 col-xs-9">
 						                                <select name="role_id" id="role_id" class="form-control select2-default" required>
 						                                    @foreach( App\Models\Base\Rol::getRoles() as $key => $value)
 						                                        <option value="{{ $key }}">{{ $value }}</option>
 						                                    @endforeach
 						                                </select>
 						                            </div>
-						                            <div class="form-group col-sm-1 col-xs-3">
+						                            <div class="form-group col-md-2 col-xs-3">
 						                                <button type="submit" class="btn btn-success btn-sm btn-block">
 						                                    <i class="fa fa-plus"></i>
 						                                </button>
