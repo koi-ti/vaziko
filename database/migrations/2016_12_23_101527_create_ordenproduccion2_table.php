c2899c434672e4af63dffc1b8395a97b7ecfb820
Pedro Camargo <dropecamargo@gmail.com>
2017-04-02 12:12:41 -0500

    Migracion detalle ordenes de produccion - produccion

--- database/migrations/2016_12_23_101527_create_ordenproduccion2_table.php
+++ database/migrations/2016_12_23_101527_create_ordenproduccion2_table.php
@@ -25,6 +25,7 @@ class CreateOrdenproduccion2Table extends Migration
             $table->string('orden2_round_formula', 10);
             $table->double('orden2_precio_venta')->default(0);
             $table->integer('orden2_entregado')->default(0);
+            $table->integer('orden2_facturados')->default(0);
             $table->text('orden2_observaciones')->nullable();
 
             $table->boolean('orden2_tiro')->default(false);
