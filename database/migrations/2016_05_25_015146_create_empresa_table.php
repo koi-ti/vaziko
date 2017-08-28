0c3445c0c1f2d2df8360c0eff060fd4347ba8543
machadoo00 <machadoo00@hotmail.com>
2017-08-28 09:06:33 -0500

    guardar bug -prueba

--- database/migrations/2016_05_25_015146_create_empresa_table.php
+++ database/migrations/2016_05_25_015146_create_empresa_table.php
@@ -17,20 +17,22 @@ class CreateEmpresaTable extends Migration
 
             $table->increments('id');
             $table->integer('empresa_tercero')->unsigned();
+            $table->string('empresa_cc_representante', 15)->nullable();
+            $table->string('empresa_nm_representante', 200)->nullable();
             $table->integer('empresa_niif')->nullable()->comment = '1 - Plena, 2 - Pymes, 3 - Micro pymes';
+            $table->integer('empresa_iva')->unsigned();
             $table->string('empresa_cc_contador', 15)->nullable();
-            $table->string('empresa_tj_contador', 15)->nullable();
-            $table->string('empresa_nm_contador', 200)->nullable();
-            $table->string('empresa_cc_revisor', 15)->nullable();
-            $table->string('empresa_tj_revisor', 15)->nullable();
-            $table->string('empresa_nm_revisor', 200)->nullable();
             $table->double('empresa_base_ica_compras')->default(0);
             $table->double('empresa_base_ica_servicios')->default(0);
             $table->double('empresa_base_retefuente_factura')->default(0);
             $table->double('empresa_porcentaje_retefuente_factura')->default(0);
             $table->double('empresa_base_reteiva_factura')->default(0);
             $table->double('empresa_porcentaje_reteiva_factura')->default(0);
-            $table->integer('empresa_iva')->unsigned();
+            $table->string('empresa_tj_contador', 15)->nullable();
+            $table->string('empresa_nm_contador', 200)->nullable();
+            $table->string('empresa_cc_revisor', 15)->nullable();
+            $table->string('empresa_tj_revisor', 15)->nullable();
+            $table->string('empresa_nm_revisor', 200)->nullable();
 
             $table->foreign('empresa_tercero')->references('id')->on('koi_tercero')->onDelete('restrict');
         });
