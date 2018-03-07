a71f392ed043859cec41220aafa50e67cc14305a
machadoo00 <machadoo00@hotmail.com>
2018-03-07 17:25:31 -0500

    charts -produccion

--- resources/views/production/ordenes/show.blade.php
+++ resources/views/production/ordenes/show.blade.php
@@ -21,6 +21,7 @@
                     <li><a href="#tab_despachos" data-toggle="tab">Distribución por clientes</a></li>
                     @if( Auth::user()->ability('admin', 'opcional2', ['module' => 'ordenes']) )
                         <li><a href="#tab_tiemposp" data-toggle="tab">Tiempos de producción</a></li>
+                        <li><a href="#tab_tiempospcharts" data-toggle="tab">Gráficas de producción</a></li>
                     @endif
                     <li class="pull-right">
                         <button type="button" class="btn btn-block btn-danger btn-sm export-ordenp">
@@ -267,6 +268,63 @@
                                             {{-- Render content productos --}}
                                         </tbody>
                                     </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane" id="tab_tiempospcharts">
+                            <div class="box box-solid">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="box box-primary">
+                                                <div class="box-header with-border">
+                                                    <h3 class="box-title">Empleado</h3>
+                                                    <div class="box-tools pull-right">
+                                                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                                                    </div>
+                                                </div>
+                                                <div class="box-body">
+                                                    <div class="chart-container">
+                                                        <canvas class="chart-line" width="500" height="200"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="box box-primary">
+                                                <div class="box-header with-border">
+                                                    <h3 class="box-title">Áreas</h3>
+                                                    <div class="box-tools pull-right">
+                                                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                                                    </div>
+                                                </div>
+                                                <div class="box-body">
+                                                    <div class="row">
+                                                        <div class="chart-container">
+                                                            <canvas class="chart-donut" width="400" height="200"></canvas>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="box box-primary">
+                                                <div class="box-header with-border">
+                                                    <h3 class="box-title">Información adicional</h3>
+                                                </div>
+                                                <div class="box-body">
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <p>Tiempo total gastado en la orden: <b class="tiempo-total"></b> minutos</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
