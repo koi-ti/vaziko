2e345c3da98661e3085ba3fb7b13c572e907fbe7
machadoo00 <machadoo00@hotmail.com>
2018-04-27 10:09:42 -0500

    cambios precotizacion -produccion

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -1082,12 +1082,12 @@
 
 		<div class="form-group col-md-2 col-xs-4">
 			<label for="producto_ancho" class="control-label">Ancho (Metros)</label>
-            <input type="number" id="producto_ancho" name="producto_ancho" value="<%- producto_ancho %>" placeholder="Ancho" step="0.1" class="form-control input-sm" <%- !parseInt(producto_metrado) ? 'readonly': ''%> required>
+            <input type="number" id="producto_ancho" name="producto_ancho" value="<%- producto_ancho %>" placeholder="Ancho" step="0.01" class="form-control input-sm" <%- !parseInt(producto_metrado) ? 'readonly': ''%> required>
             <div class="help-block with-errors"></div>
 		</div>
 		<div class="form-group col-md-2 col-xs-4">
 			<label for="producto_largo" class="control-label">Largo (Metros)</label>
-            <input type="number" id="producto_largo" name="producto_largo" value="<%- producto_largo %>" placeholder="Ancho" step="0.1" class="form-control input-sm" <%- !parseInt(producto_metrado) ? 'readonly': ''%> required>
+            <input type="number" id="producto_largo" name="producto_largo" value="<%- producto_largo %>" placeholder="Ancho" step="0.01" class="form-control input-sm" <%- !parseInt(producto_metrado) ? 'readonly': ''%> required>
             <div class="help-block with-errors"></div>
 		</div>
 	</div>
