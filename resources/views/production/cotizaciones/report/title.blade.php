2a0f535ef4d171bbc7d003c3b116545832868a16
KOI TI <soporte.koi.ti@gmail.com>
2020-11-17 18:44:28 -0500

    cambio ruta logo

--- resources/views/production/cotizaciones/report/title.blade.php
+++ resources/views/production/cotizaciones/report/title.blade.php
@@ -13,7 +13,7 @@
 			</div>
 		</td>
 		<td rowspan="8" align="center" valign="middle">
-			<img src="{{ asset(config('koi.app.image.header-pdf')) }}" alt="" style="width: 235px; height: 115px;">
+			<img src="http://181.52.238.27:8087/images/logo-header-pdf.png" alt="" style="width: 235px; height: 115px;">
 		</td>
 		<th class="border-tbl size-8" align="left">Fecha</th>
 		<th class="border-tbr size-8" align="right">{{ $cotizacion->cotizacion1_fecha_inicio }}</th>
