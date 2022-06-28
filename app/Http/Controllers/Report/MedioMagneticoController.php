f700fed139d816bfdd001c3b640a667bffe04e11
Alejandro Hernández <j.ahr@hotmail.com>
2022-06-28 11:09:24 -0500

    rmediomagnetico

--- app/Http/Controllers/Report/MedioMagneticoController.php
+++ app/Http/Controllers/Report/MedioMagneticoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Accounting\SaldoTercero, App\Models\Accounting\SaldoContable;
 
 class MedioMagneticoController extends Controller
 {
@@ -17,7 +18,18 @@ class MedioMagneticoController extends Controller
     public function index(Request $request)
     {
         if ($request->has('type')) {
-            dd( $request->all() );
+            // Preparar datos reporte
+            $mes = $request->filter_mes;
+            $ano = $request->filter_ano;
+
+            if ($mes == 1) {
+                $mes2 = 13;
+                $ano2 = $ano - 1;
+            } else {
+                $mes2 = $mes - 1;
+                $ano2 = $ano;
+            }
+
         }
         return view('reports.accounting.mediomagnetico.index');
     }
