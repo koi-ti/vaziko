5dda56e4df7bde14d7d930a5d10d538ca749ef71
Alejandro Hernández <j.ahr@hotmail.com>
2022-06-28 13:04:07 -0500

    excel

--- app/Http/Controllers/Report/MedioMagneticoController.php
+++ app/Http/Controllers/Report/MedioMagneticoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Accounting\SaldoTercero, App\Models\Accounting\SaldoContable;
+use Excel;
 
 class MedioMagneticoController extends Controller
 {
@@ -30,8 +31,38 @@ class MedioMagneticoController extends Controller
                 $ano2 = $ano;
             }
 
+            $query = SaldoContable::query();
+            $query->where('saldoscontables_mes', '=', $mes);
+            $query->where('saldoscontables_ano', '=', $ano);
+            $data = $query->get();
+
+            $tercero = null;
+            $title = sprintf('%s %s %s', $tercero ? 'Medios magneticos terceros ' : 'Medios magneticos ',  config('koi.meses')[$request->filter_mes], $request->filter_ano);
+            $titleDownload = $tercero ? 'medios_magneticos_tercero' : 'medios_magneticos';
+            $type = $request->type;
+
+            // Generate file
+            switch ($type) {
+                case 'xls':
+                    if ($tercero) {
+                       
+                    } else {
+                        Excel::create(sprintf('%s_%s_%s_%s', $titleDownload, $request->filter_ano, $request->filter_mes, date('Y_m_d H_i_s')), function ($excel) use ($data, $title, $type, $tercero) {
+                            $excel->sheet('Excel', function ($sheet) use ($saldos, $title, $type, $tercero) {
+                                $sheet->loadView('reports.accounting.mediomagnetico.report.normal', compact('data', 'title', 'type', 'tercero'));
+                            });
+                        })->download('xls');
+                    }
+                    break;
+
+                case 'pdf':
+                    $pdf = new BalanceGeneral('L', 'mm', 'Letter');
+                    $pdf->buldReport($saldos, $title);
+                    break;
+            }
         }
+
+
         return view('reports.accounting.mediomagnetico.index');
     }
-
 }
